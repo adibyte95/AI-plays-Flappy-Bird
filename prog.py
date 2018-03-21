99393160bcc555a2b9436767f8039088cde619d5
adibyte95 <singhaditya08aug@gmail.com>
2018-03-21 15:33:51 +0530

    chapter 3

--- prog.py
+++ prog.py
@@ -26,20 +26,29 @@ def car(x,y):
 
 x = display_width * 0.45
 y = display_height * 0.80
- 
+x_change = 0 
 # initially we are not crashed
 crashed = False
 
 while not crashed:
-    #this creates a list of events per frame 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-             crashed = True
-    gameDisplay.fill(white)
-    car(x,y)
-    pygame.display.update()
-    # this takes frames per second as input
-    clock.tick(60)
+	#this creates a list of events per frame 
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			crashed = True
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				x_change = -5
+			elif event.key == pygame.K_RIGHT:
+				x_change = 5
+		if event.type ==pygame.KEYUP:
+			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				x_change = 0
+	x  += x_change
+	gameDisplay.fill(white)
+	car(x,y)
+	pygame.display.update()
+	# this takes frames per second as input
+	clock.tick(60)
 
 #pygame quits 
 pygame.quit()
