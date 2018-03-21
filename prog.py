20e5a122281d306b430533958746890b6f19c47d
adibyte95 <singhaditya08aug@gmail.com>
2018-03-21 15:41:11 +0530

    chapter 4

--- prog.py
+++ prog.py
@@ -12,7 +12,7 @@ black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 
-
+car_width = 73
 #setting up the caption of the window
 pygame.display.set_caption('A bit Racey')
 
@@ -24,32 +24,38 @@ carImg = pygame.image.load('car-image.png')
 def car(x,y):
     gameDisplay.blit(carImg, (x,y))
 
-x = display_width * 0.45
-y = display_height * 0.80
-x_change = 0 
-# initially we are not crashed
-crashed = False
+def game_loop():
+	x = display_width * 0.45
+	y = display_height * 0.80
+	x_change = 0 
+	# initially we are not crashed
+	gameExit = False
 
-while not crashed:
-	#this creates a list of events per frame 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			crashed = True
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				x_change = -5
-			elif event.key == pygame.K_RIGHT:
-				x_change = 5
-		if event.type ==pygame.KEYUP:
-			if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-				x_change = 0
-	x  += x_change
-	gameDisplay.fill(white)
-	car(x,y)
-	pygame.display.update()
-	# this takes frames per second as input
-	clock.tick(60)
+	while not gameExit:
+		#this creates a list of events per frame 
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				gameExit = True
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_LEFT:
+					x_change = -5
+				elif event.key == pygame.K_RIGHT:
+					x_change = 5
+			if event.type ==pygame.KEYUP:
+				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+					x_change = 0
+		x  += x_change
+		gameDisplay.fill(white)
+		car(x,y)
+		if x >display_width - car_width or x< 0:
+			gameExit = True
 
+		pygame.display.update()
+		# this takes frames per second as input
+		clock.tick(60)
+
+#running game loop
+game_loop()
 #pygame quits 
 pygame.quit()
 #python quit
